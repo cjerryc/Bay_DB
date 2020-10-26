4d5cddeacb302430a743a225f17aa4838291b6db
ogencer <ogencer2@illinois.edu>
2020-10-25 22:51:57 -0500

    m

--- app.py
+++ app.py
@@ -1,51 +1,26 @@
 # app.py
-from flask import Flask, request, jsonify
-import connectToDB
+from flask import Flask, request, jsonify, render_template
+from connectToDB import getUser
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {param} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
 
 # A welcome message to test our server
+@app.route('/index')
 @app.route('/')
 def index():
     return "<h1>Welcome to our CS411 Project Website !!</h1> <p>by Ozgur Gencer and Ryohei Namiki</p>"
+
+
+@app.route('/hello/')
+@app.route('/hello/<username>')
+def hello(username=None):
+    try:
+        results = getUser(username)
+        return render_template('welcome.html', name=results[0], lastname=results[1])
+    except:
+        return render_template('welcome.html', name=None, lastname=None)
+
+
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
--- connectToDB.py
+++ connectToDB.py
@@ -1,6 +1,20 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
+#DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+conn = psycopg2.connect(
+        host="ec2-52-200-134-180.compute-1.amazonaws.com",
+        database="d6ehld1vcv1vv1",
+        user="vuxncunnoferwp",
+        password="bab2d1e1b712cbc8c6a16c50213d0d5888c7b53c74ea961aba4a708f55e36115")
+cur = conn.cursor()
+
+def getUser(username, cursor=cur):
+    username = "'" + username + "'"
+    query = 'SELECT firstname, lastname, email FROM users WHERE users.username = %s;' % username
+    print(query)
+    cur.execute(query)
+    return cur.fetchone()
