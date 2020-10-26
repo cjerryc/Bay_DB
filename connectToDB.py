19878a7d65f2b7b0dad04cd639ca105174f5020b
ogencer <ogencer2@illinois.edu>
2020-10-26 00:00:57 -0500

    m

--- connectToDB.py
+++ connectToDB.py
@@ -9,11 +9,7 @@ try:
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 except:
-    conn = psycopg2.connect(
-        host="ec2-52-200-134-180.compute-1.amazonaws.com",
-        database="d6ehld1vcv1vv1",
-        user="vuxncunnoferwp",
-        password="bab2d1e1b712cbc8c6a16c50213d0d5888c7b53c74ea961aba4a708f55e36115")
+    conn = None
 cur = conn.cursor()
 
 def getUser(username, cursor=cur):
