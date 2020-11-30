2ec8397a5bcaf6571eb4e5c6b47c366ca412b014
ogencer <ogencer2@illinois.edu>
2020-11-29 18:40:02 -0600

    m

--- scheduler.py
+++ scheduler.py
@@ -22,32 +22,15 @@ except:
         password="bab2d1e1b712cbc8c6a16c50213d0d5888c7b53c74ea961aba4a708f55e36115")
 cur = conn.cursor()
 
-def createTask(taskname, assignedto, repeat, usernotes, cursor=cur):
-    exists = 'false'
-    groupid = 71 ##need to chnage
-    taskid = random.randrange(1,2147483647) #id range
-    taskname = "'" + taskname + "'"
-    assignedto = "'" + assignedto + "'"
-    #need to get groupid select from group table
-    #groupid = "'" + groupid + "'"
-    status = "'incomplete'"
+def useFunct(tID, cur=cur, conn=conn):
     now = datetime.now()
     date = str(now.strftime("%m/%d/%Y"))
     time = str(now.strftime("%H:%M"))
-    #date = 'none'
-    #time = 'none'
-    usernotes = "'" + usernotes + "'"
     date = "'" + date + "'"
     time = "'" + time + "'"
-    repeat = "'" + repeat + "'"
-    query = 'INSERT INTO tasks_table(taskid, taskname, date, time, status, assignedto, completeddate, completedtime, doneby, groupid, subtasks, materials, notes) VALUES (%s, %s, %s, %s, %s, %s, NULL, NULL, NULL, %s, NULL, NULL, %s);' % (taskid, taskname, date, time, status, assignedto, groupid, usernotes)
-    cursor.execute(query)
+    query = 'SELECT upRecurrTask(%s, %s, %s);' %(tID, date, time)
+    cur.execute(query)
     conn.commit()
-    print(repeat)
-    if "No" not in repeat:
-        query = 'INSERT INTO recurring_table(taskid,taskname,repeattime,assignedto,groupid,subtasks,materials,notes) VALUES ( %s, %s, %s, %s, %s,NULL, NULL, %s) ;'% (taskid, taskname, repeat, assignedto, groupid, usernotes)
-        cursor.execute(query)
-        conn.commit()
 
 def getRecurringTasks(curs=cur):
     query = 'SELECT * FROM recurring_table;'
@@ -61,10 +44,9 @@ def getRecurringTasks(curs=cur):
             day = day.replace('(', '')
             day = day.replace(')', '')
         if (sched == "Daily"):
-            print(i)
-            #createTask(i[1], i[3], "No", "", cursor=curs)
+            useFunct(i[0])  
         if (sched == "Weekly"):
-            print(sched, day)
             if (datetime.today().strftime('%A') == day):
-                #createTask(i[1], i[3], "No", "", cursor=curs)
+                useFunct(i[0]) 
+
 getRecurringTasks()
