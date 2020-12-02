62bebf89e52c372fbd50faccfaa8406ea72405f7
lucyqz2 <lucyqz2@illinois.edu.com>
2020-12-02 08:47:40 -0600

    hoho

--- connectToDB.py
+++ connectToDB.py
@@ -14,6 +14,7 @@ client = pymongo.MongoClient("mongodb+srv://ogencer2:iWMOdvjfmgaKTLmO@cluster0.i
 mydb = client["BayDB"]
 mycol = mydb["BayDB"]
 mysub = mydb["Subtasks"]
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 #test = mycol.find_one()
 #print(test)
 try:
@@ -157,7 +158,7 @@ def searchHistory(taskname, date_created, assignedto, date_completed, doneby, cu
     stringquery = stringquery + query[len(query) - 1]
     
     print(items)
-    finalquery = (stringquery % items)
+    finalquery = ((stringquery + ';') % items)
     print(stringquery)
     print(finalquery)
     cur.execute(finalquery)
@@ -897,6 +898,9 @@ def updateTask(taskname, cursor=cur):
 
 def findSubtasks(word, mysub=mysub):
     # word = "/^" + str(word) + "/"
+    global stop_words
+    if word in stop_words:
+        return []
     word = ".*" + word + ".*"
     #{ "$regex": /^word/}
     # query = {"keyword": word }, {"_id": 0, "subtask": 1}
@@ -907,7 +911,8 @@ def findSubtasks(word, mysub=mysub):
     return array
 
 def findMaterials(word, mysub=mysub):
-    if len(word) < 3:
+    global stop_words
+    if word in stop_words:
         return []
     word = ".*^" + word + ".*"
     array = []
